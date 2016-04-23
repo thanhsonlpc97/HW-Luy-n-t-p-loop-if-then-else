8569bf972d9da6278dcb7b64d7833596284c7570
thanhsonlpc97 <thanhsonlpc97@users.noreply.github.com>
2016-04-23 18:46:02 +0700

    Update BT1.py

--- BT1.py
+++ BT1.py
@@ -1,1 +1,7 @@
-
+number = int(input('Input a number between 5 and 10:'))
+while number < 5 or number > 10:
+    number = int(input('Input a number between 5 and 10:'))
+for i in range(1, number + 1):
+    for j in range(1, i + 1):
+        print(j, end='')
+    print('')
